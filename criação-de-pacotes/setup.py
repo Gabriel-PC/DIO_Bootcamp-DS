604ffb472ea1871def8a33331bec30857f0f0460
Gabriel PC <115167796+Gabriel-PC@users.noreply.github.com>
2022-11-07 19:46:12 -0300

    Update setup.py

--- criação-de-pacotes/setup.py
+++ criação-de-pacotes/setup.py
@@ -15,7 +15,7 @@ setup(
     description="Teste para criação de pacotes",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Gabriel-PC/DIO_Bootcamp-DS/tree/main/cria%C3%A7%C3%A3o-de-pacotes"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
